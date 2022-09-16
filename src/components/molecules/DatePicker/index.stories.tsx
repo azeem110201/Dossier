17962fc030994e90ffa48edc3ffa8be7fe987eee
azeem110201 <mohd.abazeem@gmail.com>
2022-09-16 10:41:33 +0530

    Date picker added

---
+++ src/components/molecules/DatePicker/index.stories.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import ResponsiveDatePickers from ".";
+export default {
+  title: "Molecules/DatePicker",
+  component: ResponsiveDatePickers,
+} as ComponentMeta<typeof ResponsiveDatePickers>;
+
+const Template: ComponentStory<typeof ResponsiveDatePickers> = (args) => (
+  <ResponsiveDatePickers {...args} />
+);
+
+export const datePicker = Template.bind({});
+datePicker.args = {
+  label: "Year of joining",
+  date: new Date(),
+};
