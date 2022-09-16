fe42a7118abb46e243825ad23c6488391dd74ffe
azeem110201 <mohd.abazeem@gmail.com>
2022-09-16 06:58:37 +0530

    modified storybook configuration

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,3 +1,9 @@
+import { ThemeProvider as MUIThemeProvider } from "@mui/material/styles";
+import { ThemeProvider } from "@emotion/react";
+import theme from "../src/themes/index";
+import "./storybook.css";
+
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -7,3 +13,13 @@ export const parameters = {
     },
   },
 }
+
+export const decorators = [
+  (Story) => (
+    <MUIThemeProvider theme={theme}>
+        <ThemeProvider theme={theme}>
+          <Story />
+        </ThemeProvider>
+    </MUIThemeProvider>
+  ),
+];
