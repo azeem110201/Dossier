b4db073f1b3b8b7795669ae79f90da12511860fe
azeem110201 <mohd.abazeem@gmail.com>
2022-09-20 23:07:49 +0530

    fixed text nullable issue

--- src/pages/PortfolioPages/index.tsx
+++ src/pages/PortfolioPages/index.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import Header from "../../components/organisms/Header";
 import Portfolio from "../../components/organisms/Portfolio";
-import SideNav from "../../components/organisms/SideNavbar";
-import MoleculeInfographics from "../../components/molecules/MoleculeInfographics";
+import SideNav from "../../components/organisms/SideNav";
+import MoleculeInfographics from "../../components/molecules/Infographics";
 import { contentList } from "../../data/SideNavList";
 import { images, SearchIcon } from "../../data/InfographicsImages";
 import PortfolioTemplate from "../../components/templates/PortfolioTemplate";
